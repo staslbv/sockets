dabc7d92f18337bd79b6ab41ed36e2b7add5176d
Stas Lobanov <stas.lbv@gmail.com>
2017-02-08 12:50:39 +0200

    Prepare for heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+var PORT    = process.env.PORT || 3000;
+
+var express = require('express');
+
+var app     = express();
+
+var http    = require('http').Server(app);
+
+app.use(express.static(__dirname + '/public'));
+
+
+http.listen(PORT, function(connection){
+	console.log('server started');
+});
